b73c2442acef81da74d379a510aa514b8b10024e
Krishna Dave <krishnadave@Krishnas-MacBook-Pro-2.local>
2025-11-19 19:54:11 +0530

    port updation

--- backend/server.js
+++ backend/server.js
@@ -26,4 +26,6 @@ app.get("/", (req, res) => {
 });
 
 
-app.listen(5000, () => console.log("Server running at 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running at ${PORT}`));
